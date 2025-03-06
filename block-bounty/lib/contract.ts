15ca41b0f95e54f6dd5217626d3a5ae2dd2f3293
Dhairyash Jain <jaindhairyashj@gmail.com>
2025-03-06 18:30:31 +0530

    typo

--- block-bounty/lib/contract.ts
+++ block-bounty/lib/contract.ts
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 import {BugBountyABI, BugBountyAddress} from "./constants/Contanst"; // Adjust the path to your ABI file
 
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
-const RPC_URL = process.env.RPC_UR!; // e.g., https://rpc.ankr.com/electroneum_testnet or any network endpoint
+const RPC_URL = process.env.RPC_URL; // e.g., https://rpc.ankr.com/electroneum_testnet or any network endpoint
 
 if (!PRIVATE_KEY || !RPC_URL ) {
   throw new Error("Please set PRIVATE_KEY, RPC_URL, and BUG_BOUNTY_ADDRESS in your environment.");
