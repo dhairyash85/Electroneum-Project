1ca72cfcb6c8428f0837d4cf532dfc493632cf8d
Dhairyash Jain <jaindhairyashj@gmail.com>
2025-03-06 17:37:52 +0530

    changed env configurations

--- block-bounty/lib/services/axiosInstance.tsx
+++ block-bounty/lib/services/axiosInstance.tsx
@@ -1,6 +1,9 @@
 // axiosInstance.ts
 import axios, { AxiosInstance } from 'axios';
+import 'dotenv/config'
+console.log(process.env)
 const environment=process.env.ENVIRONMENT;
+console.log(environment)
 const axiosInstance: AxiosInstance = axios.create({
   baseURL: environment=="PROD"?"https://block-bounty-j095itfj9-dhairyash85s-projects.vercel.app/api":'http://localhost:3000/api',
   headers: {
--- block-bounty/next.config.ts
+++ block-bounty/next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  env:{
+    ENVIRONMENT: process.env.ENVIRONMENT
+  }
 };
 
 export default nextConfig;
