5fa324cb4d65ff135e7c825a64f908d850d473f8
Dhairyash Jain <jaindhairyashj@gmail.com>
2025-03-06 15:20:24 +0530

    adding prod

--- block-bounty/lib/services/axiosInstance.tsx
+++ block-bounty/lib/services/axiosInstance.tsx
@@ -1,8 +1,8 @@
 // axiosInstance.ts
 import axios, { AxiosInstance } from 'axios';
-
+const environment=process.env.ENVIRONMENT;
 const axiosInstance: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: environment=="PROD"?"https://block-bounty-j095itfj9-dhairyash85s-projects.vercel.app/api":'http://localhost:3000/api',
   headers: {
     'Content-Type': 'application/json',
   },
